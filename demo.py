c5ba7a2f6bcbb4b4212e4c350d2def42b9cc9434
dbilgin <dbilgin@imf.org>
2024-07-08 14:20:23 -0400

    Initial commit of step2

--- demo.py
+++ demo.py
@@ -1,21 +1,49 @@
 import pandas as pd
 import sys
-sys.path.append(r'C:\Users\dbilgin\OneDrive - International Monetary Fund (PRD)\prototype\mff')
+import numpy as np
+sys.path.append(r'/mff')
 
-from mff.mff.step2 import process_raw_constraints
+from mff.mff.step2 import process_raw_constraints, Reconciler
 from mff.mff.unconstrained_forecast import unconstrained_forecast
+from mff.mff.covariance_calc import calculate_residuals, raw_covariance
 
-forecast_start = 2023
+Tin = 10
+forecast_year = 2022
 lam = 100
+n_horizons = 7
+
 
 data = pd.read_excel(r'./data/input.xlsx', index_col=0, header=list(range(0, 3)), sheet_name='data').T
 constraints_raw = pd.read_excel(r'./data/input.xlsx', sheet_name='constraints', header=None, index_col=0)
 
 data.columns.name = 'variable'
-data = data.stack()
+data = data.unstack(['freq', 'subperiod'])
 
 C = process_raw_constraints(constraints_raw, index_iloc=range(0, 4))
 
+cols = data.columns  # sktime can't handle MultiIndex columns, so store for later
+data = data.T.reset_index(drop=True).T
+data_nona = data.dropna()
 
+y_hat, forecaster, fh = unconstrained_forecast(data_nona, Tin, fh=n_horizons, forecaster=None)
+resids = calculate_residuals(data_nona, forecaster, n_horizons, cols)
+W = raw_covariance(resids)
 
-unconstrained_forecast(data, forecast_start)
+y_hat.columns = cols
+data.columns = cols
+
+y_hat = y_hat.loc[forecast_year:]
+y_exog = data.loc[y_hat.index]
+
+y_hat = y_hat.stack(data.columns.names)
+y_exog = y_exog.stack(data.columns.names)
+
+reconciler = Reconciler(y_hat, y_exog, W, C, lam)
+y_adj = reconciler._fit()
+
+err = np.abs(y_adj['y'] - y_adj.drop('y', axis=1).sum(axis=1)).mean()
+print(f'Avg reconcilation error for GDP accounting: {err}')
+
+y_agg = y_adj.groupby(['freq', 'year']).mean()
+err = (y_agg.loc['q'] - y_agg.loc['a']).mean().mean()
+print(f'Avg reconciliation error for quarters: {err}')
