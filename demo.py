e81e596afe0eeb58dc63275d79dcfd69be830161
dbilgin <dbilgin@imf.org>
2024-07-18 13:02:25 -0400

    adjusted demo and loader for human-readable constraints

--- demo.py
+++ demo.py
@@ -8,15 +8,34 @@ from mff.mff.unconstrained_forecast import unconstrained_forecast
 from mff.mff.covariance_calc import calculate_residuals, raw_covariance
 from mff.mff.ecos_reader import load_excel
 from mff.mff.string_parser import generate_constraints_from_equations
-from itertools import product
 
 Tin = 10
-forecast_year = 2023
 lam = 100
-n_horizons = 7
+
+
+def calculate_state_space(ss_idx):
+    ss = ss_idx.to_frame().astype(str)
+    ss = ss['variable'] + '_' + ss['year'] + (ss['freq'] + ss['subperiod']).replace({'A1': ''})
+    return ss.tolist()
+
+
+def calculate_forecast_interval(ss):
+    return ss.get_level_values('year').min(), ss.get_level_values('year').max()
+
+
+def calculate_initial_inputs(df):
+    df_stacked = df.stack(df.columns.names, dropna=False)
+    ss_idx = df_stacked.index[df_stacked.isna()]
+
+    ss_str = calculate_state_space(ss_idx)
+    forecast_start, forecast_end = calculate_forecast_interval(ss_idx)
+    n_horizons = int(forecast_end - forecast_start) + 1
+    return ss_str, forecast_start, n_horizons
+
 
 directory = r'./data/input.xlsx'
-data, constraints_raw, state_space = load_excel(directory, data_fmt='ecos', constraints_fmt='readable')
+data, constraints_raw = load_excel(directory, data_fmt='ecos', constraint_fmt='readable')
+state_space, forecast_start, n_horizons = calculate_initial_inputs(data)
 C, b = generate_constraints_from_equations(constraints_raw.values, state_space)
 
 cols = data.columns  # sktime can't handle MultiIndex columns, so store for later
@@ -30,13 +49,13 @@ W = raw_covariance(resids)
 y_hat.columns = cols
 data.columns = cols
 
-y_hat = y_hat.loc[forecast_year:]
+y_hat = y_hat.loc[forecast_start:]
 y_exog = data.loc[y_hat.index]
 
 y_hat = y_hat.stack(data.columns.names)
 y_exog = y_exog.stack(data.columns.names)
 
-reconciler = Reconciler(y_hat, y_exog, W, C, lam, b)
+reconciler = Reconciler(y_hat, y_exog, W, C, b, lam)
 y_adj = reconciler._fit()
 
 err = np.abs(y_adj['y'] - y_adj.drop('y', axis=1).sum(axis=1)).mean()
