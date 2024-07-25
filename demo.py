989cd2b61b259d73824d942032d34a2d00c69b84
dbilgin <dbilgin@imf.org>
2024-07-25 16:44:17 -0400

    update excel writer with color output and add checksum for df updates (not fully implemented)

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import shutil
 import numpy as np
 sys.path.append(r'/mff')
 
@@ -8,7 +9,7 @@ from mff.mff.unconstrained_forecast import unconstrained_forecast
 from mff.mff.covariance_calc import calculate_residuals, raw_covariance
 from mff.mff.ecos_reader import load_excel
 from mff.mff.string_parser import generate_constraints_from_equations
-
+from sktime.utils import mlflow_sktime
 
 def calculate_state_space(ss_idx):
     ss = ss_idx.to_frame().astype(str)
@@ -21,41 +22,87 @@ def calculate_forecast_interval(ss):
 
 
 def calculate_initial_inputs(df):
-    df_stacked = df.stack(df.columns.names, dropna=False)
-    ss_idx = df_stacked.index[df_stacked.isna()]
-
-    ss_str = calculate_state_space(ss_idx)
-    forecast_start, forecast_end = calculate_forecast_interval(ss_idx)
+    na_years = df.index[df.isna().any(axis=1)]
+    forecast_start, forecast_end = calculate_forecast_interval(na_years)
     n_horizons = int(forecast_end - forecast_start) + 1
-    return ss_str, forecast_start, n_horizons
+
+
+    df_stacked = df.stack(df.columns.names, dropna=False)
+    fcast_stacked = df_stacked[df_stacked.index.get_level_values('year') >= forecast_start]
+    ss_str = calculate_state_space(fcast_stacked.index)
+
+    fixed_fcasts = fcast_stacked.dropna()
+    conditional_fcast_constraints = [f'{i} - {j}' for i, j in zip(calculate_state_space(fixed_fcasts.index), fixed_fcasts.values)]
+    return ss_str, conditional_fcast_constraints, forecast_start, n_horizons
 
 
 def write_to_excel(values_out, out_sheet='data'):
     wb = xw.Book().caller()
     sheet = wb.sheets[out_sheet]
+
+
+    num_rows = len(values_out)
+    num_cols = len(values_out[0]) if values_out else 0
+
+    # Iterate over the values and write them with formatting if necessary
+    for i in range(num_rows):
+        for j in range(num_cols):
+            cell = sheet.range((i + 1, j + 1))  # (i + 1, j + 1) is used because Excel is 1-indexed
+            if cell.value is None:  # Check if the cell is empty
+                cell.api.Font.Color = 255
+
     sheet.range('A1').value = values_out
+
+
+def make_df_checksum(df):
+    return pd.util.hash_pandas_object(df).sum()
+
+
+def create_cache(forecaster, df, dir=r'cache'):
+    shutil.rmtree(dir)
+    os.mkdir(dir)
+    mlflow_sktime.save_model(forecaster, os.path.join(dir, 'model.pickle'))
+    checksum = make_df_checksum(df)
+    with open(os.path.join(dir, 'df_checksum.txt'), 'w') as f:
+        f.write(str(checksum))
+
+
+def check_df_unchanged(df, checksum_dir=r'.\cache\df_checksum.txt'):
+    checksum = make_df_checksum(df)
+    with open(checksum_dir) as f:
+        checkcurrent = f.read()
+    return checksum == int(checkcurrent)
 
 
 if __name__ == '__main__':
     import xlwings as xw
     import os
-    os.chdir(r'C:\Users\dbilgin\OneDrive - International Monetary Fund (PRD)\prototype')
+    os.chdir(r'C:\users\dbilgin\OneDrive - International Monetary Fund (PRD)\prototype')
 
     Tin = 10
     lam = 100
 
-    directory = r'./data/input.xlsx'
+    directory = r'./data/input.xlsm'
     xw.Book(directory).set_mock_caller()
 
     data, constraints_raw = load_excel(directory, data_fmt='ecos', constraint_fmt='readable')
-    state_space, forecast_start, n_horizons = calculate_initial_inputs(data)
-    C, b = generate_constraints_from_equations(constraints_raw, state_space)
+    state_space, conditional_constraints, forecast_start, n_horizons = calculate_initial_inputs(data)
+
+    constraints = conditional_constraints + constraints_raw
+    C, b = generate_constraints_from_equations(constraints, state_space)
 
     cols = data.columns  # sktime can't handle MultiIndex columns, so store for later
     data = data.T.reset_index(drop=True).T
     data_nona = data.dropna()
 
-    y_hat, forecaster, fh = unconstrained_forecast(data_nona, Tin, fh=n_horizons, forecaster=None)
+    # if check_df_unchanged(data):
+    #     forecaster = mlflow_sktime.load_model(r'.\cache\model.pickle')
+    #     fh = forecaster.fh
+    #
+    # else:
+    y_hat, forecaster, fh = unconstrained_forecast(data, Tin, fh=n_horizons, forecaster=None)
+        # create_cache(forecaster, data)
+
     resids = calculate_residuals(data_nona, forecaster, n_horizons, cols)
     W = raw_covariance(resids)
 
